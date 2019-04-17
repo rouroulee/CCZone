d0c9ec94c78eccf32371537dbba27d409cb2a7be
liyanwei <liyanwei@huiyihuiying.com>
2019-04-17 19:06:42 +0800

    Initial commit

---
+++ experiment/src/main/java/com/even/experiment/ExperimentApplication.java
@@ -0,0 +1,16 @@
+package com.even.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyanwei
+ */
+@SpringBootApplication
+public class ExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExperimentApplication.class, args);
+    }
+
+}
